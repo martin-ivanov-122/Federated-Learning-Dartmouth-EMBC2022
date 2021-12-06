92448c2a8f418e802b5b9c870c03288d4eea0734
Binh Nguyen <binh.nguyen@ryerson.ca>
2021-12-06 17:16:13 -0500

    Final MVBC Function Complete

--- MVBC FCT.py
+++ MVBC FCT.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# # MVBC Alg
 
-################################################################
-# MULTI-VIEW BI-CLUSTERING ALGORITHM IMPLEMENTED IN PYTHON
-# BY: BINH NGUYEN 
-# https://github.com/binhnguyens
+# ## Functions and Libraries
+# 
 
-# Note:  
-# - matmul is * in MATLAB
-# - \* is .* in MATLAB
-################################################################
+# In[1]:
 
-# In[Libraries]
-
-################################
-# Functions and Libraries
-################################
 
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 from scipy.io import loadmat
 import copy
 
 
-# In[Update u function]:
+# # Functions in MVBC
+# 
+# Note: 
+# 
+# - matmul is * in MATLAB
+# - \* is .* in MATLAB
+
+# Something wrong in the update_u function double check
+
+# In[2]:
+
 
 ################ For testing purposes, use the following metrics: ################
 # M = M[0]
@@ -57,7 +60,7 @@ def update_u(M, z, u, v, gamma):
     return output
 
 
-# In[Update V function]:
+# In[3]:
 
 
 ################ For testing purposes, use the following metrics: ################
@@ -98,7 +101,7 @@ def update_v(M, z, u, v, gamma, s):
     
     return output
     
-# In[Update z function]:
+# In[]:
 
 ################ For testing purposes, use the following metrics: ################
     
@@ -156,7 +159,7 @@ def update_z (M,z,u,v,gamma,s):
 
 
 
-# In[Objective function]:
+# In[]:
     
 ################ OBJECTIVE FUNCTION ################
 
@@ -182,7 +185,7 @@ def objective (M,z,U,V):
     return obj
 
 
-# In[Mapping function]:
+# In[4]:
 
 
 ################ For testing purposes, use the following metrics: ################
@@ -207,7 +210,7 @@ def map_python(u, s):
     return output
 
 
-# In[Normalization]:
+# In[5]:
 
 
 # NORMALIZATION
@@ -215,142 +218,180 @@ def map_python(u, s):
 def normalization (x):
     return ((x-min(x))/(max(x)-min(x)))
 
+# def objective (M,z,U,V):
+    
+#     obj = 0;
+    
+#     for i in range (len (M)):
+#         mat =  M{i} - (U(:, i) .* z) * V{i}';            
+#         obj = obj + trace(mat' * mat)
+                          
 
 
+# # Code
 
-# In[mvlrrl0 fct]:
+# In[6]:
 
-def mvlrrl0(M,sz,sv,iSeedV1):
-    # DEBUG FCT
-    debug = 0
+
+# What is set-up in the main fct of the MATLAB file
+
+path = '../'
+filename = 'M.mat'
+signal = loadmat(path+filename)
+M = signal['M'][0]
+
+'''
+signal['M'][0] # M Var
+signal['M'][0][0] # M Var First Cell
+signal['M'][0][0][0] # M Var First Cell First Row
+'''
+
+sz1 = 9
+sv1 = np.array([[5, 5, 5]]).T
+ini_v1 = 1
+
+sz2 = 7
+sv2 = np.array([[5, 5, 5]]).T
+ini_v2 = 1
+
+
+# # mvlrrl0
+
+# In[7]:
+
+# DEBUG FCT
+debug = 0
+
+
+# Inside the mvlrrl0 function
+sz = sz1
+sv = sv1
+iSeedV1 = ini_v1
+
+
+# line 1-50
+
+maxIter = 1e03
+threshold = 1e-05
+
+n = M[0].shape[0]
+nView = len(M)
+
+d = np.zeros((nView, 1))
+z = np.ones((n,1)); # initialize z with all ones
+U = np.ones((n, nView)); # initialize u with all ones
+V = np.full([nView,1], None) 
+# V = [None]*nView
+
+gamma_z = 1.2;
+gamma_u = 1.2;
+gamma_v = 1.2;
+
+
+# In[8]:
+
+
+# line 51-60
+
+for iView in range (nView):
+    d[iView] = M[iView].shape [1]
+    V[iView] = [np.ones ((int(d[iView]),1))]
+
+
+# In[9]:
+
+
+# line 62-68
+
+# Arguments > 3
+# Initialize v1 with all zeros except 1 for the seed feature
+
+V[0][0][0:len(V[0][0])] = 0
+V[0][0][0] = 1
+
+
+# In[10]:
+
+
+# line 70-74
+
+# Did not do update v because we expect 3 arguments
+
+U[:,0] = update_u(M[0], z, U[:,0], V[0], gamma_u)
+
+tmp = map_python (U[:,0], sz)
+
+z[tmp == 0] = 0
+
+
+# line 76 - 83
+
+if (nView > 1):
+    for iView in range  (1,nView):
+        V[iView] = [update_v (M[iView], z, U[:, iView], V[iView], gamma_v, sv[iView][0])]
+        U[:, iView] = update_u(M[iView], z, U[:, iView], V[iView], gamma_u)
+
+    # Update Z
+    z = update_z (M,z,U,V,gamma_z,sz)     
+
+
+# line 85 
+
+obj = objective (M,z,U,V)
+
+
+# line 90
+
+for iTer in range (int (maxIter)):
+    pre_obj =  copy.copy(obj)
     
-    # In[line 1-50]
+    pre_z =  copy.copy(z)
     
-    maxIter = 1e03
-    threshold = 1e-05
-    
-    n = M[0].shape[0]
-    nView = len(M)
-    
-    d = np.zeros((nView, 1))
-    z = np.ones((n,1)); # initialize z with all ones
-    U = np.ones((n, nView)); # initialize u with all ones
-    V = np.full([nView,1], None) 
-    # V = [None]*nView
-    
-    gamma_z = 1.2;
-    gamma_u = 1.2;
-    gamma_v = 1.2;
-    
-    
-    # In[line 51-60]:
-    
-    
+    # Given z, update U and V
     for iView in range (nView):
-        d[iView] = M[iView].shape [1]
-        V[iView] = [np.ones ((int(d[iView]),1))]
+        V[iView] = [update_v (M[iView], z, U[:, iView], V[iView], gamma_v, sv[iView][0])]
+        U[:, iView] = update_u(M[iView], z, U[:, iView], V[iView], gamma_u)
+        
     
+    # Given U, V, update z
+    z = update_z (M,z,U,V,gamma_z,sz)     
     
-    # In[line 62-68]:
-    
-    
-    # Arguments > 3
-    # Initialize v1 with all zeros except 1 for the seed feature
-    
-    V[0][0][0:len(V[0][0])] = 0
-    V[0][0][0] = 1
-    
-    
-    # In[line 70-74]:
-    
-    
-    # Did not do update v because we expect 3 arguments
-    
-    U[:,0] = update_u(M[0], z, U[:,0], V[0], gamma_u)
-    
-    tmp = map_python (U[:,0], sz)
-    
-    z[tmp == 0] = 0
-    
-    
-    # line 76 - 83
-    
-    if (nView > 1):
-        for iView in range  (1,nView):
-            V[iView] = [update_v (M[iView], z, U[:, iView], V[iView], gamma_v, sv[iView][0])]
-            U[:, iView] = update_u(M[iView], z, U[:, iView], V[iView], gamma_u)
-    
-        # Update Z
-        z = update_z (M,z,U,V,gamma_z,sz)     
-    
-    
-    # line 85 
-    
+    # Calc the objective fct
     obj = objective (M,z,U,V)
     
+    if (debug == 1):
+        print ('Iteration: %d \n' %iTer)
+        print ('Objective value %f \n' %obj)
+        
+    if (np.linalg.norm(pre_z-z) < threshold):
+        break
     
-    # line 90
     
-    for iTer in range (int (maxIter)):
-        pre_obj =  copy.copy(obj)
-        
-        pre_z =  copy.copy(z)
-        
-        # Given z, update U and V
-        for iView in range (nView):
-            V[iView] = [update_v (M[iView], z, U[:, iView], V[iView], gamma_v, sv[iView][0])]
-            U[:, iView] = update_u(M[iView], z, U[:, iView], V[iView], gamma_u)
-            
-        
-        # Given U, V, update z
-        z = update_z (M,z,U,V,gamma_z,sz)     
-        
-        # Calc the objective fct
-        obj = objective (M,z,U,V)
-        
-        if (debug == 1):
-            print ('Iteration: %d \n' %iTer)
-            print ('Objective value %f \n' %obj)
-            
-        if (np.linalg.norm(pre_z-z) < threshold):
-            break
-        
-        
-    return (z,U,V,obj)
-    
-        
-        
-    
-# In[MAIN FUNCTION]
 
-'''
-Sample code can be seen below
-'''
-
-# path = '../'
-# filename = 'M.mat'
-# signal = loadmat(path+filename)
-# M = signal['M'][0]
-
-# '''
-# signal['M'][0] # M Var
-# signal['M'][0][0] # M Var First Cell
-# signal['M'][0][0][0] # M Var First Cell First Row
-# '''
-
-# sz1 = 9
-# sv1 = np.array([[5, 5, 5]]).T
-# ini_v1 = 1
-
-# sz2 = 7
-# sv2 = np.array([[5, 5, 5]]).T
-# ini_v2 = 1
-
-# z,U,V,obj = mvlrrl0 (M,sz1,sv1,ini_v1)
 
     
     
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
